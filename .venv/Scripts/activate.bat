7e9f7621dc62f195b6fd1b5984e23a6a738f925d
Subhajit Mondal <79186750+SUBHAJITMONDAL-03@users.noreply.github.com>
2023-06-13 03:05:33 +0530

    first

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\prase\OneDrive\Desktop\Subhajit\PwC_Internship\Csv_pdf_txt\PwCbot\.venv
+set VIRTUAL_ENV=C:\Users\prase\OneDrive\Desktop\Subhajit\PwC_Internship\Csv_pdf_txt\PwCbot (4)\PwCbot\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
